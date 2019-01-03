4adab1a01635fb80942d2728ed50d15157ba0935
huangsansui <674208615@qq.com>
2019-01-03 19:39:21 +0800

    Add shell sort

---
+++ Sort/src/ShellSort.java
@@ -0,0 +1,45 @@
+/**
+ * Function:
+ *
+ * 希尔算法
+ * 根据需求，如果你想要结果从大到小排列，它会首先将数组进行分组，然后将较大值移到前面，较小值
+ * 移到后面，最后将整个数组进行插入排序，这样比起一开始就用插入排序减少了数据交换和移动的次数，可以说希尔排序是加强
+ * 版的插入排序
+ * 拿数组5, 2, 8, 9, 1, 3，4来说，数组长度为7，当increment为3时，数组分为两个序列
+ * 5，2，8和9，1，3，4，第一次排序，9和5比较，1和2比较，3和8比较，4和比其下标值小increment的数组值相比较
+ * 第一次后increment的值变为3/2=1,此时对数组进行插入排序，
+ * 实现数组从小到大排
+ * @author Huangqing
+ * @date 2019/1/3 18:35
+ * @since JKD 1.8
+ */
+public class ShellSort {
+
+    public void sort(int[] numbers) {
+        int j = 0;
+        int temp = 0;
+        //每次将步长缩短为原来的一半
+        for (int increment = numbers.length / 2; increment > 0; increment /= 2) {
+            for (int i = increment; i < numbers.length; i++) {
+                temp = numbers[i];
+                for (j = i; j >= increment; j -= increment) {
+                    if (temp < numbers[j - increment]) {
+                        numbers[j] = numbers[j - increment];
+                    }else {
+                        break;
+                    }
+                }
+                numbers[j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ShellSort shellSort = new ShellSort();
+        int[] numbers = new int[]{4, 1, 8, 6, 2};
+        shellSort.sort(numbers);
+        for (int n : numbers) {
+            System.out.print(n + " ");
+        }
+    }
+}
